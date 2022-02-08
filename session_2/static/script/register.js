8d70d1c35bf6997cdb084347c45941866cd4210b
TreySchneider95 <tschneid95@gmail.com>
2022-02-08 16:08:44 -0700

    day 3

--- session_2/static/script/register.js
+++ session_2/static/script/register.js
@@ -11,7 +11,6 @@ let salon={
   },
   pets : []
 }
-
 // let pets = [{
 //   petName:'Lucy',
 //   age:3,
@@ -50,42 +49,46 @@ function consolePets(petRegister){
 }
 // consolePets(pets)
 
-function PetReg(petName, age, gender, breed, service, owner){
+function PetReg(petName, age, gender, breed, service, owner, contact){
   this.petName=petName
   this.age=age
   this.gender=gender
   this.breed=breed
   this.service=service
   this.owner=owner
+  this.contact=contact
 }
-let dog1 = new PetReg('Spot', 12, 'M', 'mix', 'trim', 'Bill')
-let dog2 = new PetReg('Tank', 4, 'M', 'mix', 'trim', 'Ralph')
-let dog3 = new PetReg('Sally', 12, 'F', 'mix', 'trim', 'Nancy')
+let dog1 = new PetReg('Spot', 12, 'M', 'mix', 'trim', 'Bill', '389-408-2786')
+let dog2 = new PetReg('Tank', 4, 'M', 'mix', 'trim', 'Ralph', '635-837-9086')
+let dog3 = new PetReg('Sally', 12, 'F', 'mix', 'trim', 'Nancy', '749-984-9032')
 salon.pets.push(dog1, dog2, dog3)
 // console.log(dog1.petName)
+let TableLables= ['Name', 'Age', 'Gender', 'Breed', 'Service', 'Owner', 'Contact']
+let formFields = ['petName', 'petAge', 'petService', 'petOwner', 'petBreed', 'petContact']
+function displayNames(arr) {
+  document.getElementById('lables').innerHTML = null
+  for (i = 0; i < TableLables.length; i++) {
+    document.getElementById('lables').innerHTML += `<td>${TableLables[i]}</td>`
+    }
+    document.getElementById('table-info').innerHTML = null
+  for (x=0;x<arr.length;x++){
+    document.getElementById('table-info').innerHTML += `<tr><td>${arr[x].petName}</td><td>${arr[x].age}</td><td>${arr[x].gender}</td><td>${arr[x].breed}</td><td>${arr[x].service}</td><td>${arr[x].owner}</td><td>${arr[x].contact}</td></tr>`
+  }
+    // table.innerHTML = ``
+}
 
 function register(){
-  console.log('register')
   let petName=document.getElementById('txt-petName').value
   let petAge=document.getElementById('txt-petAge').value
   let petGender=document.getElementById('txt-petGender').value
+  let petBreed=document.getElementById('txt-petBreed').value
   let petService=document.getElementById('txt-petService').value
   let petOwner=document.getElementById('txt-petOwner').value
-  let petObject = new PetReg(petName, petAge, petGender, petService, petOwner)
-  document.getElementById('txt-petName').value = null
-  document.getElementById('txt-petAge').value = null
-  document.getElementById('txt-petGender').value = null
-  document.getElementById('txt-petService').value = null
-  document.getElementById('txt-petOwner').value = null
-  console.log(petObject)
+  let petContact=document.getElementById('txt-petContact').value
+  let petObject = new PetReg(petName, petAge, petGender, petBreed, petService, petOwner, petContact)
+  for(x=0;x<formFields.length;x++){
+    document.getElementById(`txt-${formFields[x]}`).value = null
+  }
   salon.pets.push(petObject)
+  displayNames(salon.pets)
 }
-
-function displayNames(arr) {
-	for (i = 0; i < arr.length; i++) {
-    console.log('something')
-		document.getElementById('registered-pets').innerHTML += `<li>${arr[i].petName}</li>`
-	}
-}
-console.log(salon)
-displayNames(salon.pets)
